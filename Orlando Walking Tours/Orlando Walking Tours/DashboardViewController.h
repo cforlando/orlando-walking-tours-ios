d51e7912c86dd82a85190e19b42b1b423f3754fc
Andrew Kozlik <andrew@codefortravel.com>
2015-01-31 15:23:27 -0500

    Add MagicalRecord submodule

---
+++ Orlando Walking Tours/Orlando Walking Tours/DashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.h
+//  Orlando Walking Tours
+//
+//  Created by Andrew Kozlik on 1/31/15.
+//  Copyright (c) 2015 Andrew Kozlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardViewController : UIViewController
+
+@end
