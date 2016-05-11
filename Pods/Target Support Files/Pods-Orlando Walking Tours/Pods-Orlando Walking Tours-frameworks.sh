657d5bd3848af6b23b7efed58f7d677d13953114
Keli'i Martin <kelii.d.martin@gmail.com>
2016-05-10 22:13:36 -0400

    Added MagicalRecord; updated all Pods

--- Pods/Target Support Files/Pods-Orlando Walking Tours/Pods-Orlando Walking Tours-frameworks.sh
+++ Pods/Target Support Files/Pods-Orlando Walking Tours/Pods-Orlando Walking Tours-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Orlando Walking Tours/Alamofire.framework"
+  install_framework "Pods-Orlando Walking Tours/MagicalRecord.framework"
   install_framework "Pods-Orlando Walking Tours/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Orlando Walking Tours/Alamofire.framework"
+  install_framework "Pods-Orlando Walking Tours/MagicalRecord.framework"
   install_framework "Pods-Orlando Walking Tours/SwiftyJSON.framework"
 fi
