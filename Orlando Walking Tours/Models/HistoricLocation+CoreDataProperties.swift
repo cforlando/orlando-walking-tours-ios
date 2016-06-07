7e55ec52766c6709a568904d9b2337a6f0c31152
Keli'i Martin <kelii.d.martin@gmail.com>
2016-06-07 17:14:02 -0400

    Fixing another typo
    Just a minor typo fix (historicLocations instead of historiclocations)

--- Orlando Walking Tours/Models/HistoricLocation+CoreDataProperties.swift
+++ Orlando Walking Tours/Models/HistoricLocation+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  HistoricLocation+CoreDataProperties.swift
 //  Orlando Walking Tours
 //
-//  Created by Keli'i Martin on 6/4/16.
+//  Created by Keli'i Martin on 6/7/16.
 //  Copyright © 2016 Code for Orlando. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -15,15 +15,15 @@ import CoreData
 extension HistoricLocation {
 
     @NSManaged var address: String?
+    @NSManaged var id: NSNumber?
     @NSManaged var latitude: NSNumber?
     @NSManaged var localRegistryDate: NSDate?
     @NSManaged var locationDescription: String?
     @NSManaged var locationTitle: String?
     @NSManaged var locationType: String?
     @NSManaged var longitude: NSNumber?
-    @NSManaged var sortOrder: NSNumber?
-    @NSManaged var id: NSNumber?
     @NSManaged var nrhpDate: NSDate?
+    @NSManaged var sortOrder: NSNumber?
     @NSManaged var tour: Tour?
 
 }
